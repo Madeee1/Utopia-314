27f31b7535f3d5bde5cb96d4a35607d2224d778c
Madeee1 <madeee@duck.com>
2024-04-28 13:17:24 +0000

    feat: signup works, route to signin on success

--- server/models/user.schema.ts
+++ server/models/user.schema.ts
@@ -1,38 +1,43 @@
-import { defineMongooseModel } from '#nuxt/mongoose'
-import bcrypt from 'bcrypt';
-import { Document, Types } from 'mongoose';
+import { defineMongooseModel } from "#nuxt/mongoose";
+import bcrypt from "bcrypt";
+import { Document, Types } from "mongoose";
 
 export const userSchema = defineMongooseModel({
-    name: "User",
-    schema: {
-        username: {
-            type: String,
-            required: true,
-        },
-        email: {
-            type: String,
-            required: true,
-        },
-        hashPassword: {
-            type: String,
-            required: true,
-        },
-        // @ts-ignore
-        uuid: {
-            type: Types.ObjectId,
-            required: true,
-            unique: true,
-            default: () => new Types.ObjectId(),
-        }
+  name: "User",
+  schema: {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
     },
-    hooks(schema) {
-        // Add a pre-save hook to hash the password
-        schema.pre('save', async function (next) {
-            const user: any = this
-            if (!user.isModified('hashPassword')) return next()
-            // Hash the password
-            user.hashPassword = await bcrypt.hash(user.hashPassword, 10)
-            return next()
-        })
+    email: {
+      type: String,
+      required: true,
     },
-})
+    hashPassword: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      required: true,
+    },
+    // @ts-ignore
+    uuid: {
+      type: Types.ObjectId,
+      required: true,
+      unique: true,
+      default: () => new Types.ObjectId(),
+    },
+  },
+  hooks(schema) {
+    // Add a pre-save hook to hash the password
+    schema.pre("save", async function (next) {
+      const user: any = this;
+      if (!user.isModified("hashPassword")) return next();
+      // Hash the password
+      user.hashPassword = await bcrypt.hash(user.hashPassword, 10);
+      return next();
+    });
+  },
+});
