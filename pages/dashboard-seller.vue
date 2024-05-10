729c88ec51f73d438ff61d61b379ebf058befe74
Madeee1 <madeee@duck.com>
2024-05-10 09:36:03 +0000

    feat: css changes on seller dashboard

--- pages/dashboard-seller.vue
+++ pages/dashboard-seller.vue
@@ -33,7 +33,10 @@
         >
         <p class="card-text">Views: {{ listing.views }}</p>
         <p class="card-text">Shortlist Count: {{ listing.shortlistNumber }}</p>
-        <button class="btn" @click="showReviewModal(listing.userId)">
+        <button
+          class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
+          @click="showReviewModal(listing.userId)"
+        >
           Rate and Review my agent
         </button>
       </div>
