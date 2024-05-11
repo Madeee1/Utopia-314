a546303b72ce59af9b3f10019d54ae0f8b422c6e
studentcyber1 <93244098+studentcyber1@users.noreply.github.com>
2024-05-12 04:41:04 +0800

    Add files via upload

---
+++ server/models/profile.schema.ts
@@ -0,0 +1,13 @@
+import { defineMongooseModel } from "#nuxt/mongoose";
+import { Document, Types } from "mongoose";
+
+export const profileSchema = defineMongooseModel({
+  name: "Profile",
+  schema: {
+    profile: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+});
