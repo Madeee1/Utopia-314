9d1cefd0e30cecdf6276e26b5c8ee2f962ba1dae
Abdul Majeed <141847064+AMajeed786@users.noreply.github.com>
2024-05-13 08:19:17 +0000

    no changes

--- pages/dashboard-seller.vue
+++ pages/dashboard-seller.vue
@@ -36,8 +36,7 @@
         <button
           class="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded"
           @click="showReviewModal(listing.userId)"
-        >
-          Rate and Review my agent
+        >Rate and Review my agent
         </button>
       </div>
     </div>
